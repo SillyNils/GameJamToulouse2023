79f5bc17a0de49c0f20bf272a957a11bd27bd89b
SillyNils <churlaudnils@gmail.com>
2023-04-01 17:13:21 +0200

    Add SismicEvent

--- Assets/Scripts/MotherEvent.cs
+++ Assets/Scripts/MotherEvent.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MotherEvent : MonoBehaviour
 {
+    public GeneralRotation generalRotation;
+
     [Header("Timing parameter")]
     [SerializeField] private float _timing;
 
@@ -22,38 +24,35 @@ public class MotherEvent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //SismicEventTrigger(_timing, _amplitude, _frequency);
     }
 
-    
+
     void SismicEventTrigger(float timing, float amplitude, float frequency)
     {
-        // param timing : durée d'action dans la SafeZone
-
+        // param timing : duree d'action dans la SafeZone 
+        float initXspeed = 100f;
+        generalRotation.YspeedRotation = Mathf.Clamp(initXspeed + amplitude * 0.01f * Mathf.Sin(frequency * 0.01f * Time.time),50f,150f);
     }
 
     void SolarFlareEventTrigger(float timing, float amplitude, float delay)
     {
         // param timing : durée de l'event
-
     }
 
     void MeteorologicEventTrigger(float timing, float amplitude, float delay)
     {
         // param timing : durée de l'event
-
     }
 
     void TsunamiEventTrigger(float timing, float amplitude, float _maxTravelDistance)
     {
         // param timing : durée de l'event
-
     }
 
 }
