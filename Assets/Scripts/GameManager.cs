827d83c81285076bed2ba26d171e802b8b02f40b
Skilda <87537199+Skilda@users.noreply.github.com>
2023-04-02 17:55:02 +0200

    qf

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Time.timeScale = 1.0f;
+
         _GameOverPanel.SetActive(false);
     }
 
